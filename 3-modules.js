768e61102ff872bb4dcaac9bbc16f30946bfbd42
Htet Aung Lin <htetlin099798@gmail.com>
2024-04-03 19:59:53 +0630

    add first module

---
+++ 3-modules.js
@@ -0,0 +1,11 @@
+//  Modules
+const { htet, aung } = require("./4-names");
+const sayHi = require("./5-util");
+const person = require("./6-alternative-flavor");
+
+// console.log(person);
+require("./7-mind-grenade");
+
+// sayHi("Htet Aung Lin");
+// sayHi(htet);
+// sayHi(aung);
---
+++ 4-names.js
@@ -0,0 +1,7 @@
+// local
+const secret = "SUPER SECRET";
+// share
+const htet = "htet";
+const aung = "aung";
+
+module.exports = { htet, aung };
