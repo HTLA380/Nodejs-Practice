c0309968b36badfb34b31826f87ca93e4814c102
Htet Aung Lin <htetlin099798@gmail.com>
2024-04-03 19:41:40 +0630

    add module setup

---
+++ 3-module-setup.js
@@ -0,0 +1,12 @@
+//  Modules
+
+const htet = "htet";
+const aung = "aung";
+
+const sayHi = (name) => {
+  console.log(`Hello there ${name}`);
+};
+
+sayHi("Htet Aung Lin");
+sayHi(htet);
+sayHi(aung);
