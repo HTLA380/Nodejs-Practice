b08f5a2315f59b2ad6d44b02671ae60d225c10ac
Htet Aung Lin <htetlin099798@gmail.com>
2024-04-11 10:17:19 +0630

    remove unnecessary files

--- expressjs/1-http-basic.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-
-const server = http.createServer((req, res) => {
-  console.log("user enter the server");
-  // we need to send the info to the browser
-  res.end("Hello World");
-});
-
-server.listen(5000);
--- expressjs/2-http-header.js
+++
@@ -1,8 +0,0 @@
-const http = require("http");
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "content-type": "text/plain" });
-  res.end("<h1>Hello World</h1>");
-});
-
-server.listen(5000);
