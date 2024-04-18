d05a2d4258054e269426455036718342af6f5791
Htet Aung Lin <htetlin099798@gmail.com>
2024-04-18 19:56:08 +0630

    add book-directory final version

--- books-directory/controllers/books.js
+++ books-directory/controllers/books.js
@@ -2,7 +2,60 @@ const BookModel = require("../model/Book");
 
 const getAllBooks = async (req, res) => {
   try {
-    const books = await BookModel.find({});
+    const { name, author, genre, sort, fields, numericFilters } = req.query;
+    const queryObject = {};
+
+    if (name) {
+      queryObject.name = { $regex: name, $options: "i" };
+    }
+    if (author) {
+      queryObject.author = { $regex: author, $options: "i" };
+    }
+    if (genre) {
+      queryObject.genre = { $regex: genre, $options: "i" };
+    }
+
+    if (numericFilters) {
+      const operatorMap = {
+        ">": "$gt",
+        ">=": "$gte",
+        "=": "$eq",
+        "<": "$lt",
+        "<=": "$lte",
+      };
+      const regEx = /\b(<|>|>=|=|<|<=)\b/;
+      let filters = numericFilters.replace(regEx, (match) => `-${operatorMap[match]}-`);
+
+      const options = ["price"];
+      filters = filters.split(",").forEach((item) => {
+        const [field, operator, value] = item.split("-");
+        if (options.includes(field)) {
+          queryObject[field] = { [operator]: Number(value) };
+        }
+      });
+    }
+
+    let result = BookModel.find(queryObject);
+
+    if (sort) {
+      const sortList = sort.split(",").join(" ");
+      result = result.sort(sortList);
+    } else {
+      result = result.sort("date");
+    }
+
+    if (fields) {
+      const fieldsList = fields.split(",").join(" ");
+      result = result.select(fieldsList);
+    }
+
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    result = result.skip(skip).limit(limit);
+
+    const books = await result;
     res.json({ books });
   } catch (error) {
     throw new Error(error);
@@ -12,7 +65,7 @@ const getAllBooks = async (req, res) => {
 const getSingleBook = async (req, res) => {
   try {
     const { id: bookID } = req.params;
-    const book = await BookModel.findById({ _id: bookID });
+    const book = await BookModel.findOne({ _id: bookID });
     if (!book) {
       throw new Error(`Book with id: ${bookID} not found`);
     }
