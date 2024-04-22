f5f9204c1fad57bfc8ae6ffff1597ae4a97e0080
Htet Aung Lin <htetlin099798@gmail.com>
2024-04-23 06:28:07 +0630

    feat(backend): add basic error handler

--- blog-app/server/app.js
+++ blog-app/server/app.js
@@ -6,13 +6,18 @@ const app = express();
 const blogRouter = require("./routes/blog");
 const connectDB = require("./db/connect");
 
+const BadRequest = require("./errors/bad-request");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/api/", (req, res) => {
+  throw new BadRequest("this is a bad request error");
   res.send("Welcome to my new blog post");
 });
 
 app.use("/api/blogs", blogRouter);
+app.use(errorHandlerMiddleware);
 
 const PORT = 8080;
 
