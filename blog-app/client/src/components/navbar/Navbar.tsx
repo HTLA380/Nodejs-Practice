38f2429bb1ee08a2ced4d217c9fec756cc1fe309
Htet Aung Lin <htetlin099798@gmail.com>
2024-04-26 15:10:06 +0630

    style(client/navbar): center navbar

--- blog-app/client/src/components/navbar/Navbar.tsx
+++ blog-app/client/src/components/navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import ThemeToggler from "../theme-toggler/ThemeToggler";
 const Navbar = () => {
   return (
     <header className="w-full flex items-center py-7 px-5">
-      <nav className="flex items-center justify-between py-2 w-full max-w-6xl ">
+      <nav className="mx-auto flex items-center justify-between py-2 w-full max-w-6xl ">
         <h3>BuddhaWisdom</h3>
         <ul className="flex items-center gap-7">
           {["blog", "projects", "about", "newsletter"].map((navlink) => (
