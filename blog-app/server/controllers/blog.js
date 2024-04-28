6229ff2b76b4bbbed1d68d3a88d56f38d4892bd1
Htet Aung Lin <htetlin099798@gmail.com>
2024-04-28 16:04:36 +0630

    feature(backend/blogs): add sorting and api limit

--- blog-app/server/controllers/blog.js
+++ blog-app/server/controllers/blog.js
@@ -3,7 +3,7 @@ const { StatusCodes } = require("http-status-codes");
 const { NotFoundError } = require("../errors");
 
 const getBlogs = async (req, res) => {
-  const { fields, author, category } = req.query;
+  const { fields, author, category, sort } = req.query;
 
   let queryObject = {};
 
@@ -17,10 +17,23 @@ const getBlogs = async (req, res) => {
 
   let result = blogModel.find(queryObject);
 
+  if(sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList)
+  } else {
+    result = result.sort("createdAt")
+  }
+
   if (fields) {
-    const fieldsList = req.query.fields.split(",").join(" ");
+    const fieldsList = fields.split(",").join(" ");
     result = result.select(fieldsList);
   }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 9;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
 
   const blogs = await result;
   res.status(StatusCodes.OK).json({ blogs });
